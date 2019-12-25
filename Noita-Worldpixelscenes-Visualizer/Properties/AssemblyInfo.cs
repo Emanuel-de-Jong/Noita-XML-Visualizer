cc2009942ca4887b8ac0862209a7c7f22346cc36
Emanuel de Jong <44051853+killbottt@users.noreply.github.com>
2019-12-25 12:57:36 +0100

    a lot

--- Noita-Worldpixelscenes-Visualizer/Properties/AssemblyInfo.cs
+++ Noita-Worldpixelscenes-Visualizer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NoitaWorldPixelScene")]
+[assembly: AssemblyTitle("Noita-Worldpixelscenes-Visualizer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NoitaWorldPixelScene")]
+[assembly: AssemblyProduct("Noita-Worldpixelscenes-Visualizer")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c98fb3e2-3edc-4269-8977-d6f13863c2ac")]
+[assembly: Guid("7865cf2b-868d-4c8c-b4f4-23aeffd74bb6")]
 
 // Version information for an assembly consists of the following four values:
 //
